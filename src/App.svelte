3d87cc508a73f07349f3443f30a14442fdc7084c
chaituvarma-109 <chaitanyavarma.u@gmail.com>
2023-08-01 02:42:00 +0530

    score counter webapp

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import PlayerScore from "./PlayerScore.svelte";
+
+  let player1Score = 0;
+  let player2Score = 0;
+
+  function reset() {
+    player1Score = 0;
+    player2Score = 0;
+  }
+</script>
+
+<main>
+  <PlayerScore bind:playerScore={player1Score} playerName='player1' />
+  <PlayerScore bind:playerScore={player2Score} playerName='player2' />
+  <button on:click={reset} class="contrat outline">Reset</button>
+</main>
---
+++ src/PlayerScore.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let playerName;
+    export let playerScore = 0;
+
+    function decreaseScore() {
+        playerScore--
+        if (playerScore < 0) {
+            playerScore = 0
+        }
+    }
+</script>
+
+<div>
+    <h3 class="center">{playerName}</h3>
+    <button on:click={() => playerScore++}>up</button>
+    <h1 class="center">{playerScore}</h1>
+    <button on:click={decreaseScore}>down</button>
+</div>
+
+<style>
+    .center{
+        text-align: center;
+    }
+</style>
